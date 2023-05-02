044ec3ee229da6941c948081dbf60992c22849e0
Mirgiyos Turganov <mirgiyosiddin.turganov@tendable.com>
2023-05-02 17:04:17 +0300

    telegram

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,11 +5,11 @@ Telegram::Bot::Client.run(ENV["TOKEN"]) do |bot|
   # Listen for messages sent to your bot
   bot.listen do |message|
     # Save the message to the database
-    Message.create(
-      user_id: message.from.id,
-      text: message.text,
-      date: Time.at(message.date)
-    )
+    # Message.create(
+    #   user_id: message.from.id,
+    #   text: message.text,
+    #   date: Time.at(message.date)
+    # )
 
     # Send a response back to the user
     bot.api.send_message(
