4197ca517ade9d146e13001afae00e06b38d0a7e
Mirgiyos Turganov <mirgiyosiddin.turganov@tendable.com>
2023-05-02 16:42:19 +0300

    init

---
+++ app/bot/bot.rb
@@ -0,0 +1,20 @@
+require 'telegram/bot'
+
+# Initialize the Telegram bot with your API token
+Telegram::Bot::Client.run('TOKEN') do |bot|
+  # Listen for messages sent to your bot
+  bot.listen do |message|
+    # Save the message to the database
+    Message.create(
+      user_id: message.from.id,
+      text: message.text,
+      date: Time.at(message.date)
+    )
+
+    # Send a response back to the user
+    bot.api.send_message(
+      chat_id: message.chat.id,
+      text: "Thanks for your message!"
+    )
+  end
+end
